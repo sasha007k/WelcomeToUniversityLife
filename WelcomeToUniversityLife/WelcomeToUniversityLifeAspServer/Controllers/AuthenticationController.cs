e4d16fc4fce861979e70ac7a0575b378559a7926
Mishka Sorochinskiy <mishash20161506@gmail.com>
2020-03-07 12:50:10 +0200

    added auth contraller with register anf sign in methods ; added register and sign in models; added associated with actions views

---
+++ WelcomeToUniversityLife/WelcomeToUniversityLifeAspServer/Controllers/AuthenticationController.cs
@@ -0,0 +1,38 @@
+﻿using Application.Models.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WelcomeToUniversityLifeAspServer.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public AuthenticationController()
+        {
+
+        }
+
+        [HttpGet]
+        public  IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromBody]RegisterModel request)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignIn([FromBody]SignInModel request)
+        {
+            return View();
+        }
+    }
+}
