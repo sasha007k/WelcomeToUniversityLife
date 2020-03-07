2e8d59a4326644039ed686dff7c60ca191adba92
bitcher8 <nikita.saakyan@gmail.com>
2020-03-07 13:04:10 +0200

    add User Entity

---
+++ WelcomeToUniversityLife/Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class User: IdentityUser<int>
+    {
+     public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string City { get; set; }
+        public int UserInfoID { get; set; }
+    
+    }
+}
