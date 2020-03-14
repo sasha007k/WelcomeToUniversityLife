1381e49934943cf9f9c4bcc2ec78824bd71aea9e
bitcher8 <nikita.saakyan@gmail.com>
2020-03-14 13:35:50 +0200

    add univerity,speciality,documnt config

---
+++ WelcomeToUniversityLife/Infrastructure/Configurations/DocumentConfiguartions.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Configuration
+{
+    public class DocumentConfiguration : IEntityTypeConfiguration<Document>
+    {
+        public void Configure(EntityTypeBuilder<Document> builder)
+        {
+            builder.ToTable("Document");
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Name)
+                .IsRequired();
+           
+
+        }
+    }
+}
