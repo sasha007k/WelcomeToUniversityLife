c44f05fe058db9e6278923354ed90ce657a3b185
bitcher8 <nikita.saakyan@gmail.com>
2020-03-30 11:56:51 +0300

    fix footer

--- WelcomeToUniversityLife/Infrastructure/Migrations/20200328221709_init.cs
+++ WelcomeToUniversityLife/Infrastructure/Migrations/20200328221709_init.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Infrastructure.Migrations
 {
-    public partial class InitialMigration : Migration
+    public partial class init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
