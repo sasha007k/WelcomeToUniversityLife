656bff0b739ef2de75176146fae052ef9f89e87a
bitcher8 <nikita.saakyan@gmail.com>
2020-04-07 13:17:06 +0300

    validate subjects

--- WelcomeToUniversityLife/WelcomeToUniversityLifeAspServer/wwwroot/js/site.js
+++ WelcomeToUniversityLife/WelcomeToUniversityLifeAspServer/wwwroot/js/site.js
@@ -1,1 +1,31 @@
-﻿
+﻿function doSomething(obj) {
+
+
+    var btnValue = obj.options[obj.selectedIndex].value;
+    var x = obj.name
+
+    if (x === 'secondsub') {
+        remove('foursub', btnValue);
+        remove('thirdsub', btnValue)
+
+    } else if (x === 'thirdsub') {
+
+        remove('secondsub', btnValue);
+        remove('foursub', btnValue)
+    } else if (x === 'foursub') {
+        remove('secondsub', btnValue);
+        remove('thirdsub', btnValue)
+
+    }
+
+}
+
+
+function remove(id, value) {
+    var selectobject = document.getElementById(id);
+    for (var i = 0; i < selectobject.length; i++) {
+        if (selectobject.options[i].value == value)
+            selectobject.remove(i);
+    }
+
+}
