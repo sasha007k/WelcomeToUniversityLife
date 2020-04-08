f15763cbbf73d65710d11561160df92cab2cb3fa
Kryklyvets Oleksandr <sashakryklyvets@gmail.com>
2020-04-08 14:27:03 +0300

    Fixed zno subject selection

--- WelcomeToUniversityLife/WelcomeToUniversityLifeAspServer/wwwroot/js/site.js
+++ WelcomeToUniversityLife/WelcomeToUniversityLifeAspServer/wwwroot/js/site.js
@@ -1,37 +1,36 @@
 ﻿function doSomething(obj) {
 
-
     var btnValue = obj.options[obj.selectedIndex].value;
     var x = obj.name
-    alert(x)
 
-    if (x === 'MarksModel.SecondZno.Name') {
-        remove('MarksModel.FourZno.Name', btnValue);
-        remove('MarksModel.ThreedZno.Mark', btnValue)
+    if (x === 'MarksModel.SecondZnoModel.Name') {
+        remove('MarksModel.FourZnoModel.Name', btnValue);
+        remove('MarksModel.ThirdZnoModel.Name', btnValue);
 
-    } else if (x === 'MarksModel.ThreedZno.Mark') {
+    } else if (x === 'MarksModel.ThirdZnoModel.Name') {
+        remove('MarksModel.SecondZnoModel.Name', btnValue);
+        remove('MarksModel.FourZnoModel.Name', btnValue);
 
-        remove('MarksModel.SecondZno.Name', btnValue);
-        remove('MarksModel.FourZno.Name', btnValue)
-    } else if (x === 'MarksModel.FourZno.Name') {
-        remove('MarksModel.SecondZno.Name', btnValue);
-        remove('MarksModel.ThreedZno.Mark', btnValue)
+    } else if (x === 'MarksModel.FourZnoModel.Name') {
+        remove('MarksModel.SecondZnoModel.Name', btnValue);
+        remove('MarksModel.ThirdZnoModel.Name', btnValue);
 
     }
-function uploadphotoclicked() {
-    let fileinput = document.getElementById("fileinputid");
+}
 
-    fileinput.onchange = function (e) {
-        document.getElementById("photoform").submit();
-    }
-
-
-    function remove(id, value) {
+function remove(id, value) {
         var selectobject = document.getElementById(id);
         for (var i = 0; i < selectobject.length; i++) {
             if (selectobject.options[i].value == value)
                 selectobject.remove(i);
         }
+}
 
+
+function uploadphotoclicked() {
+    let fileinput = document.getElementById("fileinputid");
+
+    fileinput.onchange = function (e) {
+        document.getElementById("photoform").submit();
     }
 }
